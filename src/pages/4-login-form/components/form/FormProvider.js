b1d067a905fb343a54b69b0642e4dc93893bd8e0
snowoverflow <203398696@qq.com>
2017-02-24 17:34:28 +0800

    ...

--- src/pages/4-login-form/components/form/FormProvider.js
+++ src/pages/4-login-form/components/form/FormProvider.js
@@ -1,25 +1,28 @@
 /**
  * Created by Yun on 2016-12-10.
  */
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 
 export default class FormProvider extends Component {
-  static propTypes = {
-    // eslint-disable-next-line react/forbid-prop-types
-    form: PropTypes.object,
-    children: PropTypes.element.isRequired,
-  };
-
-  static childContextTypes = {
-    form: PropTypes.object,
-  };
-
-  getChildContext() {
-    return {
-      form: this.props.form,
+    static propTypes = {
+        // eslint-disable-next-line react/forbid-prop-types
+        form: PropTypes.object,
+        children: PropTypes.element.isRequired,
     };
-  }
-  render() {
-    return React.Children.only(this.props.children);
-  }
+
+    //此处的数据可以越级传递，子组件都能通过定义 contextTypes 访问 context 中的数据```this.context.form
+    //http://www.tuicool.com/articles/m2muAbU
+    static childContextTypes = {
+        form: PropTypes.object,
+    };
+
+    getChildContext() {
+        return {
+            form: this.props.form,
+        };
+    }
+
+    render() {
+        return React.Children.only(this.props.children);//返回唯一的子元素否则报错
+    }
 }
